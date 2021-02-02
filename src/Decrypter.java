18545e5e6d4198d07f185da490c04f1dc8abb0a4
brun0-znx <69119889+brun0-znx@users.noreply.github.com>
2021-02-02 05:06:06 -0300

    uploading files

--- src/Decrypter.java
+++ src/Decrypter.java
@@ -6,7 +6,8 @@ public class Decrypter {
     }
     /** This method runs the decrypter, it receives a string and separate it in a string vector using
      * spaces between words to split, checks the biggest word in the vector so can be used by stopping condition and then
-     * goes through the vector getting the characters of the words in it, in order.
+     * goes through the vector getting the characters of the words in it, in order. I tryed to decrypt using the same idea
+     * of the encrypting method, but I found some difficulties so I decided to do it in another way.
      * @param msg, with the string that was in the input file.
      * @return a decrypted string, it will be used by the streaming class.
      */
@@ -23,7 +24,7 @@ public class Decrypter {
             }
         }
 
-        //looping to get the characters ind order
+        //looping to get the characters in order
         while (true) {
             for (int i = 0; i<vector.length; i++) {
                 try {
